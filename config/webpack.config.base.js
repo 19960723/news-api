1a0044836f14ed8751c0f7204fd2487362c2f407
lxl <1847426505@qq.com>
2020-12-22 16:58:16 +0800

    webpack dev + prod 打包配置

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,10 +1,60 @@
 const path = require('path')
-import config from './config'
-console.log(config)
-module.exports = {
-  entry: path.join(__dirname, '../src/main.js'),
+const config = require('./config')
+const webpack = require('webpack')
+const nodeExcternals = require('webpack-node-externals')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const webpackconfig = {
+  target: 'node',
+  entry: {
+    server: path.join(config.APP_PATH, 'main.js')
+  },
+  resolve: {
+    ...config.getWebpackResolveConfig()
+  },
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'my-first-webpack.bundle.js'
+    path: config.DIST_PATH,
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader'
+        },
+        exclude: [path.join(__dirname, '/node_modules')]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV:
+          process.env.NODE_ENV === 'production' ||
+            process.env.NODE_ENV === 'prod'
+            ? "'production'"
+            : "'development'"
+      }
+    })
+  ],
+  externals: [nodeExcternals()],
+  node: {
+    global: false,
+    __filename: false,
+    __dirname: false
   }
+  // node: {
+  //   console: true,
+  //   global: true,
+  //   process: true,
+  //   Buffer: true,
+  //   __filename: true,
+  //   __dirname: true,
+  //   setImmediate: true,
+  //   path: true
+  // },
 }
+
+module.exports = webpackconfig
